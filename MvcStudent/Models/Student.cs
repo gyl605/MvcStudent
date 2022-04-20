a8cf1e7fd3eb09f73686809d6fdc3fb9d925e113
gyl605 <2070250472@qq.com>
2022-04-20 17:48:21 +0800

    添加项目文件。

---
+++ MvcStudent/Models/Student.cs
@@ -0,0 +1,40 @@
+﻿namespace MvcStudent.Models;
+using System.ComponentModel.DataAnnotations;
+
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "姓名")]
+        [StringLength(300)]
+        public string? Name { get; set; }
+
+
+        [Display(Name = "性别")]
+        [StringLength(1)]
+        public string? Gender { get; set; }
+
+
+        [Display(Name = "年龄")]
+        [Range(1, 200)]
+        public int? Age { get; set; }
+
+
+        [Display(Name = "出生日期")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "体温")]
+        [Range(30, 60)]
+        public string? Temperature { get; set; }
+
+        [Display(Name = "联系方式")]
+        [StringLength(13)]
+        public string? Phone { get; set; }
+
+
+
+        [Display(Name = "记录日期")]
+        public DateTime Date { get; set; }
+    }
+
